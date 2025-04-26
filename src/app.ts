0a3adf5462d51e560b2add3240edd78421a6e94e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-26 23:14:52 +0700

    feat: add auth module to main route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { errorHandler } from "./helper/error.handler.js";
 import { logHandler } from "./helper/log.handler.js";
+import auth from './modules/auth/auth.route.js';
 import task from './modules/task/task.route.js';
 
 const app = new Hono();
@@ -9,6 +10,7 @@ app.onError(errorHandler)
 
 app.use("*", logHandler);
 
+app.route('/auth', auth)
 app.route('/task', task);
 
 app.get('/healthcheck', (c) => { 
