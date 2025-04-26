83b5e24e718edb64759d1043814fd7d0e9544476
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-26 22:21:00 +0700

    feat: add task module

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
 import { errorHandler } from "./helper/error.handler.js";
+import { logHandler } from "./helper/log.handler.js";
+import task from './modules/task/task.route.js';
 
 const app = new Hono();
 
-app.use('*', errorHandler);
+app.use(errorHandler);
+app.use(logHandler);
+
+app.route('/task', task);
 
 app.get('/healthcheck', (c) => { 
     return c.text('OK');
