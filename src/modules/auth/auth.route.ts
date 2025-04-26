0deb8dfd59958391c3d2516bf1d99ec8a5970fad
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-26 23:50:38 +0700

    feat: authenticatoin mechanism

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,14 +1,28 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { successResponse } from "../../common/response.js";
+import { loginSchema, registerSchema } from "./auth.schema.js";
+import { login, register } from "./auth.service.js";
 
 const app = new Hono();
 
-app.post("/login", (c) => { 
-    return successResponse(c, {}, "Login successful");
+app.post("/login",
+    zValidator("json", loginSchema),
+    async (c) => { 
+        const {email, password} = c.req.valid("json");
+        const responseData = await login(email, password);
+
+        return successResponse(c, responseData, "Login successful", 200);
 })
 
-app.post("/register", (c) => { 
-    return successResponse(c, {}, "Registration successful", 201);
+app.post("/register",
+    zValidator("json", registerSchema),
+    (c) => {
+        const { email, password, name } = c.req.valid("json");
+        
+        const responseData = register(email, password, name);
+
+        return successResponse(c, responseData, "Registration successful", 201);
 })
 
 export default app;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,76 @@
-export async function login(email: string, password: string) {
+import { AppError } from "@/common/error.js";
+import argon2 from "argon2";
+import { sign } from "hono/jwt";
+import prisma from "prisma/prisma.js";
 
+export async function login(email: string, password: string) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (!user) {
+        throw new AppError("Invalid email or password", 401);
+    }
+
+    const isPasswordValid = await argon2.verify(user.password, password);
+
+    if (!isPasswordValid) {
+        throw new AppError("Invalid email or password", 401);
+    }
+
+    const payload = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+    }
+
+    const token = await sign(
+        payload,
+        process.env.JWT_SECRET as string,
+    )
+
+    return {
+        ... payload,
+        token,
+    };
 }
 
 export async function register(email: string, password: string, name: string) {
-    
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (user) {
+        throw new AppError("User already exists", 409);
+    }
+
+    const hashedPassword = await argon2.hash(password);
+
+    const newUser = await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name,
+        },
+    });
+
+    const payload = {
+        id: newUser.id,
+        email: newUser.email,
+        name: newUser.name,
+    }
+
+    const token = await sign(
+        payload,
+        process.env.JWT_SECRET as string,
+    )
+
+    return {
+        ... payload,
+        token,
+    };
 }
