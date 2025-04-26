50f19817bc46aa7e415f0f96ea606b7b8a9c4ad0
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-26 23:11:46 +0700

    feat: add skeleton for task module

--- src/modules/task/task.route.ts
+++ src/modules/task/task.route.ts
@@ -1,9 +1,54 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { successResponse } from "../../common/response.js";
 import { authMiddleware } from "../../middleware/auth.middleware.js";
 import type { authenticatedRoute } from "../../types.js";
+import { createTaskSchema } from "./task.schema.js";
 
 const app = new Hono<authenticatedRoute>()
 
 app.use(authMiddleware);
+
+app.get('/', (c) => {
+    return successResponse(c, {
+
+    }, "Successfully get all tasks");
+})
+
+app.get('/:id', (c) => {
+    const { id } = c.req.param();
+    return successResponse(c, {
+        id
+    }, `Successfully get task with id ${id}`);
+})
+
+app.post('/',
+    zValidator("json", createTaskSchema),
+    (c) => {
+        const body = c.req.valid("json");
+
+        return successResponse(c, {
+        }, "Successfully create task", 201);
+    })
+
+app.put('/:id',
+    zValidator("json", createTaskSchema),
+    (c) => {
+        const { id } = c.req.param();
+        const body = c.req.valid("json");
+
+        return successResponse(c, {
+            id,
+            ...body
+        }, `Successfully update task with id ${id}`);
+    })
+    
+app.delete('/:id', (c) => {
+    const { id } = c.req.param();
+
+    return successResponse(c, {
+        id
+    }, `Successfully delete task with id ${id}`);
+})
 
 export default app;
